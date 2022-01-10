93ad583c7639b19551a2a491271481ab86ccd53b
CartHorseZ <cartman.david@gmail.com>
2022-01-11 10:35:31 +1300

    Version0.1

--- examplepriceIE.py
+++ examplepriceIE.py
@@ -28,11 +28,14 @@ def calc_discharge_rate(spot_price):
 
     print("margin=", margin)
 
+    #linear scale margin to exp value
     scaled_margin = np.interp(margin, [config.MARGIN_MIN , config.MARGIN_MAX],[config.MIN_EXP_VALUE, config.MAX_EXP_VALUE])
     
+    # apply the exp function to exp_value less the min
     scaled_margin_exp = np.exp(scaled_margin) - np.exp(config.MIN_EXP_VALUE)
     print("scaled_margin=", scaled_margin," ", "scaled_margin_exp=",scaled_margin_exp)
   
+    #Scale exp function applied value to discharge rate 
     discharge_rate = config.IE_MIN_RATE + (scaled_margin_exp*multiplier)
     print("discharge_rate=",discharge_rate)
 
@@ -41,18 +44,3 @@ calc_discharge_rate(120)
 calc_discharge_rate(130)
 calc_discharge_rate(140)
 calc_discharge_rate(150)
-
-
-# def calc_discharge_rate(spot_price):
-    
-#     margin = spot_price - avg_spot_price
-
-#     print("margin=", margin)
-
-#     scaled_margin = np.interp(margin, [config.MARGIN_MIN , config.MARGIN_MAX],[min_exp_value, max_exp_value])
-    
-#     scaled_margin_exp = np.exp(scaled_margin) - np.exp(min_exp_value)
-#     print("scaled_margin=", scaled_margin," ", "scaled_margin_exp=",scaled_margin_exp)
-  
-#     discharge_rate = config.IE_MIN_RATE + (scaled_margin_exp*multiplier)
-#     print("discharge_rate=",discharge_rate)
